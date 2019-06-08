a8b29ac213f1e68c7ecacc10b3f8d2abc14a791c
Santiago <boris96bs7@gmail.com>
2019-06-08 14:47:20 -0500

    Update componente.php
    componentes

--- pos/vistas/modulos/componente.php
+++ pos/vistas/modulos/componente.php
@@ -2,42 +2,94 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        COMPONENTES
-        <small>TABLERO DE COMPONENTES</small>
+        INTEGRANTES
+        <small>COMPONENTES DEL PROYECTO</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> INICIO</a></li>
-        <li><a href="#">COMPONENTES</a></li>
+        <li><a href="inicio"><i class="fa fa-dashboard"></i> INICIO</a></li>
+        <li><a href="#">COMPONENTES DEL PROYECTO</a></li>
       </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
-
       <!-- Default box -->
       <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
+          <div class="box-header with-border">
+              <button class="btn btn-info" data-toggle="modal" data-target="#modalAgregarCompo">
+                Agregar COMPONENTES
+              </button>
           </div>
-        </div>
-        <div class="box-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
-      </div>
-      <!-- /.box -->
 
-    </section>
-    <!-- /.content -->
+          <div class="box-body">
+            <table class="table table-bordered table-striped dt-responsive tablas ">
+              <caption>COMPONENTES DEL PROYECTO</caption>
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">Codigo Componente</th>
+                  <th scope="col">Descripcion</th>
+                  <th scope="col">Linea Base</th>
+
+                </tr>
+              </thead>
+
+
+            </table>
+          </div>
+      </div>
+   </section>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="modalAgregarCompo"  role="dialog" >
+  <div class="modal-dialog">
+    <div class="modal-content">
+     <form  role="form method="post" enctype="multipart/form-data">
+       <div class="modal-header" style="background: #39CCCC; color:white">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h5 class="modal-title" style="text-align: center;">AGREGAR COORDINADOR DE FACULTAD</h5>
+        </div>
+
+      <div class="modal-body">
+        <div class="box-body">
+            <!------------------- CEDULA DE INDENTIDAD ----------------------------------------->
+          <div class="form-group">
+              <div class="input-group">
+                  <div class="input-group-addon"><i class="fa fa-address-card"></i></div>
+                    <input type="text" class="form-control input-lg" name="cedulausuario" placeholder="Cédula" required>
+             </div>
+             <br>
+        <!-------------------------------- NOMBRE DE USUARIO --------------------------------->
+              <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                      <input type="text" class="form-control input-lg" name="nombreusuario" placeholder="Nombre del integrante" required>
+                </div>
+              </div>
+
+              <!-------------------------------- APELLIDO DEL USUARIO --------------------------------->
+              <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-addon"><i class="fa fa-user"></i></div>
+                      <input type="text" class="form-control input-lg" name="apellidousuario" placeholder="Apellido del integrante" required>
+                </div>
+              </div>
+
+
+
+         </div>
+       </div>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+        <button type="submit" class="btn btn-primary">Agregar Componente</button>
+      </div>
+
+    </form>
   </div>
+ </div>
+</div>
